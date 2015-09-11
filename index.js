a5afe6c923ec51000612585e7ce3a5d93810c113
Ethan Aubuchon <eaubuchon@i-sight.com>
2015-09-11 14:31:01 -0400

    Moved FieldSpecParser to lib folder and updated index to export module

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-var FieldSpecParser = require("./FieldSpecParser.js");
-
-var fields = FieldSpecParser.getFieldsFromWorkbook('Training 1 Field Specification V3.xlsx');
-
-console.log(fields);
+module.exports = require('./lib/FieldSpecParser');
